d1585f3b57252ec84e7f745e702d3d50bd61e32b
Amantha Gamage <134222527+AmiGamageChan@users.noreply.github.com>
2023-09-28 20:37:16 +0530

    index.php

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
+<!DOCTYPE html>
 
+<head>
+
+    <title>AJAX</title>
+
+</head>
+
+<body>
+
+    <p>X</p>
+    <input type="text" id="x"/>
+    <br><br>
+
+    <p>Y</p>
+    <input type="text" id="y"/>
+    <br><br>
+    
+    <button onclick="m();">Submit</button>
+    <br><br>
+
+    <div id="d">0</div>
+
+    <script src="script.js"></script>
+</body>
+
+</html>
